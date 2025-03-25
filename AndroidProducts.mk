7686847f3557ad3d33e5d2897f3e27a985351ad3
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-03-25 13:55:48 -0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/droidx_violet.mk
+    $(LOCAL_DIR)/omni_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    droidx_violet-ap1a-eng \
-    droidx_violet-ap1a-userdebug \
-    droidx_violet-ap1a-user
+    omni_violet-ap1a-eng \
+    omni_violet-ap1a-userdebug \
+    omni_violet-ap1a-user
