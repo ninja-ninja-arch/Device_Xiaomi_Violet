1a242f99ceccaa7bdd8aee7a75a0a1270e0a356b
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-03-26 07:29:08 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,13 @@
 # vendor & Kernel stuff
-git clone https://github.com/Drpto/vendor_xiaomi_violet.git -b 15 vendor/xiaomi/violet 
-git clone https://github.com/Drpto/kernel_xiaomi_violet.git -b 15 kernel/xiaomi/violet
+git clone https://github.com/Drpto/vendor_xiaomi_violet.git -b 15 vendor/xiaomi/violet --depth=1 
+git clone https://github.com/Drpto/kernel_xiaomi_violet.git -b 15 kernel/xiaomi/violet --depth=1 
 
 #kernel clang
 rm -rf prebuilts/clang/host/linux-x86/clang-r498229
-git clone https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r498229.git prebuilts/clang/host/linux-x86/clang-r498229
+git clone https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r498229.git prebuilts/clang/host/linux-x86/clang-r498229 --depth=1 
 
 #Firmware
-git clone https://gitlab.com/Drpto/vendor_xiaomi-firmware_violet.git -b fifteen vendor/xiaomi-firmware/violet
+git clone https://gitlab.com/Drpto/vendor_xiaomi-firmware_violet.git -b fifteen vendor/xiaomi-firmware/violet --depth=1 
 
 # Miui Camera stuff
-git clone https://gitlab.com/Joker-V2/android_vendor_MiuiCamera.git -b fifteen vendor/MiuiCamera
+git clone https://gitlab.com/Joker-V2/android_vendor_MiuiCamera.git -b fifteen vendor/MiuiCamera --depth=1 
