5f10b36ffab19b1342b15e291c0bd2c94852c2c4
ninja-ninja-arch <rajustha8027414@gmail.com>
2025-03-26 07:26:22 -0700

    Voilet :- welcome linaege

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_violet.mk
+    $(LOCAL_DIR)/lineage_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_violet-ap1a-eng \
-    omni_violet-ap1a-userdebug \
-    omni_violet-ap1a-user
+    lineage_violet-ap1a-eng \
+    lineage_violet-ap1a-userdebug \
+    lineage_violet-ap1a-user
